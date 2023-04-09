80684f248ba7edef3883db1a734df00c646d3b6c
Chris Clark <chris.clark@globalstripe.com>
2023-04-09 23:31:04 +0100

    auth updates

---
+++ setup.sh
@@ -0,0 +1,7 @@
+echo "# nxtamplifyauth" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/globalstripe/nxtamplifyauth.git
+git push -u origin main
